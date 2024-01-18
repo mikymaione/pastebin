204862c5b662b603d923da4be3222acb602c1727
Michele Maione <mikymaione@hotmail.it>
2024-01-18 10:14:06 +0100

    refactor: return 404 for option

--- src/paste/src/router.rs
+++ src/paste/src/router.rs
@@ -8,28 +8,36 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 use actix_web::{App, delete, get, HttpServer, post, Responder, Result, web};
-use actix_web::error::ErrorConflict;
+use actix_web::error::{ErrorConflict, ErrorInternalServerError, ErrorNotFound};
 
 use crate::store::PastebinStore;
 
+fn get_store() -> Result<PastebinStore> {
+    PastebinStore::new(false)
+        .map_err(ErrorInternalServerError)
+}
+
 #[get("/{id}")]
 async fn get_paste(id: web::Path<i64>) -> Result<impl Responder> {
-    let store = PastebinStore::new(false)
-        .map_err(ErrorConflict)?;
-    let maybe_pastebin = store.get(id.into_inner())
+    let id = id.into_inner();
+
+    let store = get_store()?;
+
+    let maybe_pastebin = store.get(id)
         .map_err(ErrorConflict)?;
 
-    let maybe_content = maybe_pastebin
+    let content = maybe_pastebin
         .map(|p| p.content)
-        .unwrap_or(String::from(""));
+        .ok_or(format!("No pastebin found with id: {id}"))
+        .map_err(ErrorNotFound)?;
 
-    Ok(format!("{maybe_content}"))
+    Ok(format!("{content}"))
 }
 
 #[post("/")]
 async fn set_paste(req_body: String) -> Result<impl Responder> {
-    let store = PastebinStore::new(false)
-        .map_err(ErrorConflict)?;
+    let store = get_store()?;
+
     let id = store.add(req_body)
         .map_err(ErrorConflict)?;
 
@@ -38,9 +46,11 @@ async fn set_paste(req_body: String) -> Result<impl Responder> {
 
 #[delete("/{id}")]
 async fn delete_paste(id: web::Path<i64>) -> Result<impl Responder> {
-    let store = PastebinStore::new(false)
-        .map_err(ErrorConflict)?;
-    let deleted = store.delete(id.into_inner())
+    let id = id.into_inner();
+
+    let store = get_store()?;
+
+    let deleted = store.delete(id)
         .map_err(ErrorConflict)?;
 
     Ok(format!("{deleted}"))
